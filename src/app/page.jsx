8aa1976cf688d53ac55746af7d2462775aaa3163
Gurleen <gill.gurleenkaur2004@gmail.com>
2025-03-23 21:25:44 +0530

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,19 +13,14 @@ const Homepage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        {/* IMAGE CONTAINER */}
         <div className="h-4/5 lg:h-500  lg:w-1/2 relative">
           <Image src="/myphoto.png" alt="" fill className="object-contain" />
         </div>
-        {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-          {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
           Coding Clean, Functional Designs that Work.         </h1>
-          {/* DESC */}
           <p className="md:text-xl">
 I build fast, scalable web applications with clean code, turning designs into smooth, functional user experiences.          </p>
-          {/* BUTTONS */}
           <div className="w-full flex gap-4">
             <Link href="/portfolio">
             <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
